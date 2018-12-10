4268453cf1942bfcd9d745ada2ff672ebd11c57e
Unknown <AlixAkhribi@yahoo.com>
2018-12-09 19:40:42 -0800

    Create Genesis Block and static genesis method on Block class

--- block.js
+++ block.js
@@ -1,8 +1,19 @@
+const { GENESIS_DATA} = require('./config')
+
+console.log(GENESIS_DATA);
+
+
 class Block {
-    constructor(timestamp, lastHash, hash, data) {
+    constructor({ timestamp, lastHash, hash, data }) {
         this.timestamp = timestamp;
         this.lastHash = lastHash;
         this.hash = hash;
         this.data = data;
     }
+
+    static genesis() {
+        return new Block(GENESIS_DATA);
+    }
 }
+
+module.exports = Block
